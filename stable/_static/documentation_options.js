8e6f2c1af5f82dc65ddcae967f70d78ca4318e15
bot@try.out <Circle CI>
2023-08-18 14:54:13 +0000

    Update the docs: https://circleci.com/gh/sphinx-gallery/sphinx-gallery/3626

--- stable/_static/documentation_options.js
+++ stable/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.13.0-git',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.14.0-git',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
