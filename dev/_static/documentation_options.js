fa0fa5bd58f5ea767b2211024cbb102240a00c24
bot@try.out <Circle CI>
2026-09-18 14:37:06 +0000

    Update the docs: https://circleci.com/gh/sphinx-gallery/sphinx-gallery/10358

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.23.0.dev1+g983633297-git',
+    VERSION: '0.23.0.dev2+g06e3f1405-git',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
